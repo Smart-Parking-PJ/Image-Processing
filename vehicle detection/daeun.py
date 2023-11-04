cf529b5f05d27f3fef34172f9a280898ee1edef3
yeachan <dus001228@naver.com>
2023-11-04 17:16:11 +0900

    :zap: 차량을 제외한 다른 물체 무시 #8
    차량을 제외한 다른 물체에 대한 처리를 진행하지 않아 실행시간 단축

--- vehicle detection/daeun.py
+++ vehicle detection/daeun.py
@@ -11,11 +11,10 @@ with open('vehicle detection\YOLO model\coco.names', "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # 영상 가져오기
 video_path = "Test_video/KakaoTalk_20231010_181752927.mp4"  # 영상 파일 경로
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(0)
 
 output_interval=10
 last_output_time=time.time()
@@ -42,43 +41,34 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-
+            # 차량 및 트럭일 경우에만 처리
             if confidence > 0.5:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                #if
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+                if classes[class_id] == "car" or classes[class_id] == "truck":
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
 
     # 노이즈 제거
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
+    count=0
     # 최종
-    font = cv2.FONT_HERSHEY_PLAIN  # 글꼴
     for i in range(len(boxes)):
         if i in indexes:
             x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
+            count=count+1
             confidence=confidences[i]
-            color = colors[i]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0,0,255), 2)
+                
+    print(f"현재 차량 수 : {count}")
+
+
             
-#현재 인식하는 차량 수 출력
-    current_time = time.time()
-    if current_time - last_output_time >= output_interval:
-        for i in range(len(boxes)):
-            if i in indexes:
-                label = str(classes[class_ids[i]])
-                if(label=="car"or"truck"):
-                    count=count+1
-        print(f"현재 차량 수 : {count}")
-        count=0
-        last_output_time = current_time
         
     # 영상 프레임 출력
     cv2.imshow("video", frame)
