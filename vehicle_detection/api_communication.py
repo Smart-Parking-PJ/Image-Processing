46cd0f1cfaa1b4b338e2bb28b957c6711ae4a07b
yeachan <dus001228@naver.com>
2023-11-12 22:44:48 +0900

    :truck: 변수 이름 변경 #24
    변수 이름 변경

--- vehicle_detection/api_communication.py
+++ vehicle_detection/api_communication.py
@@ -1,13 +1,13 @@
 import requests
 from api_server import api_add
 
-def api_patch(id, currentcar, image):
+def api_patch(id, car, image):
     
-    totalspace = requests.get(api_add + str(id) + "/")[totalspace]
+    ts = requests.get(api_add + str(id) + "/")["totalspace"]
 
     update_data = {
-        "currentcar": currentcar,
-        "emptyspace" : totalspace - currentcar,
+        "currentcar": car,
+        "emptyspace" : ts - car,
         "image": image,
         }
     #patch요청
