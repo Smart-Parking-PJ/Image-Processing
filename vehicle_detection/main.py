2d5228443808d02f659ad3564fc05135f7cf1b36
yeachan <dus001228@naver.com>
2023-11-28 16:50:27 +0900

    :zap: Image patch 요청 처리 기능 수정
    image patch 요청 처리 오류 수정

--- vehicle_detection/main.py
+++ vehicle_detection/main.py
@@ -2,7 +2,7 @@ import cv2
 from get_frame import frame_get
 
 
-video_path="vehicle_detection/Test_video/school3.mp4"
+video_path="vehicle_detection/Test_video/1.mp4"
 cap = cv2.VideoCapture(video_path)
 
 frame_get(cap)
