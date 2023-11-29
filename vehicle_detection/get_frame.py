4e1bc8ca66c7ee23d78f55ed75017b4a0404f118
yeachan <dus001228@naver.com>
2023-11-29 20:21:35 +0900

    :zap: Id 값 인자로 설정 #20
    Id 값 인자로 설정

--- vehicle_detection/get_frame.py
+++ vehicle_detection/get_frame.py
@@ -4,7 +4,7 @@ from yolo_nas import count_car
 from api_communication import api_patch
 
 
-def frame_get(cap):
+def frame_get(cap, id):
     last_output_time=time.time() 
     output_time=2 #5 or 10
     while True:
@@ -16,9 +16,9 @@ def frame_get(cap):
 
         if(video_current_time-last_output_time>=output_time): #n초마다 영상 캡쳐
             car = count_car(frame)
-            image_path = 'cap.jpeg'
-            saveimg = cv2.imwrite(image_path, frame)
-            api_patch(1, car, image_path)
+            image_path = 'parking_lot_' + str(id) + '.jpeg'
+            cv2.imwrite(image_path, frame)
+            api_patch(id, car, image_path)
             last_output_time=video_current_time
 
         if cv2.waitKey(1) & 0xFF == ord('a'): #종료
--- vehicle_detection/main.py
+++ vehicle_detection/main.py
@@ -5,4 +5,4 @@ from get_frame import frame_get
 video_path="vehicle_detection/Test_video/1.mp4"
 cap = cv2.VideoCapture(video_path)
 
-frame_get(cap)
+frame_get(cap, 1)
