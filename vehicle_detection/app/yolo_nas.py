8bf3e175e7d6fefbcfdd6032e0513d31533dc736
yeachan <dus001228@naver.com>
2024-03-14 11:11:37 +0900

    :sparkles: 이미지 처리 객체화 및 테스트 함수 생성 #36
    YOLO클래스 생성 및 테스트 조건문 생성

--- vehicle_detection/app/yolo_nas.py
+++ vehicle_detection/app/yolo_nas.py
@@ -2,37 +2,41 @@ import cv2
 import torch
 from super_gradients.training import models
 
-#-- GPU 설정
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
-use_cuda = torch.cuda.is_available()
-print(use_cuda)
-if use_cuda:
-  print(torch.cuda.get_device_name(0))
+class YOLO:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    use_cuda = torch.cuda.is_available()
+    print(use_cuda)
+    if use_cuda:
+        print(torch.cuda.get_device_name(0))
 
-#-- 사전학습된 Yolo_nas_small 모델 불러오기(빠르지만 정확도가 낮음)
-model1 = models.get("yolo_nas_l", pretrained_weights ="coco").to(device)
+    #-- 사전학습된 Yolo_nas_small 모델 불러오기(빠르지만 정확도가 낮음)
+    model1 = models.get("yolo_nas_l", pretrained_weights ="coco").to(device)
 
-def count_car(img):
-    # img = cv2.imread(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
- 
-    results = model1.predict(img, conf=0.25, fuse_model= False)
+    async def count_car(self, img):
+        img = cv2.imread(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    
+        results = self.model1.predict(img, conf=0.25, fuse_model= False)
 
-    for result in results :
-        labels = result.prediction.labels
+        for result in results :
+            labels = result.prediction.labels
 
-    labels = list(labels)
+        labels = list(labels)
 
-    count_car = 0
-    count_truck = 0
-    for num in labels:
-        if num == 2:
-            count_car += 1
-        elif num == 7:
-            count_truck += 1
-    cnt = count_car + count_truck
-    # print("인식된 차량의 개수: " + str(cnt))
+        count_car = 0
+        count_truck = 0
+        for num in labels:
+            if num == 2:
+                count_car += 1
+            elif num == 7:
+                count_truck += 1
+        cnt = count_car + count_truck
+        print("인식된 차량의 개수: " + str(cnt))
 
-    return(cnt)
+        return(cnt)
+    
+if __name__ == "__main__":
+    test = YOLO()
+    print(test.count_car("vehicle_detection/app/test_img/test.png"))
