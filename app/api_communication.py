3986ea165836adb83dded8f76895c92c44bc2d24
yeachan <dus001228@naver.com>
2024-04-16 14:59:09 +0900

    :fire: 빈공간 계산 기능 제거 #36

--- app/api_communication.py
+++ app/api_communication.py
@@ -3,18 +3,14 @@ from app.config import get_secret
 
 def api_patch(id, car):
     api_url = get_secret("API_SERVER")
-    ts = requests.get(api_url + str(id)).json()["totalSpace"]
     with open('pred.jpg', 'rb') as file:
         img = file.read()
         upload = {'image':img}
         update_data = {
         "currentCar": car,
-        "emptySpace" : ts - car,
         }
-
         #patch요청
-        response = requests.patch(api_url + str(id), data = update_data, files = upload)
-        print(response.status_code)
+        requests.patch(api_url + str(id), data = update_data, files = upload)
 
 if __name__ == "__main__":
     api_url = "https://93c4-223-194-160-130.ngrok-free.app/parking/1"
